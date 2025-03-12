64b92c907e8553e1bc2ca60ce847a2bdf0d2c9a4
nasrin <n.mokhtari@wiki-land.net>
2025-03-12 09:08:25 +0330

    api call

--- newbynextjs/src/app/page.jsx
+++ newbynextjs/src/app/page.jsx
@@ -1,9 +1,22 @@
-import Link from "next/link";
+async function getUser() {
+  let response = await fetch("https://jsonplaceholder.typicode.com/users");
+  let users = await response.json();
+  return users;
+}
 
-export default function Home(){
-  return(
+export default async function showUsers() {
+  let allUsers = await getUser();
+  return (
     <div>
-      <h1>This is Home Page</h1>
+      <ul>
+        {
+          allUsers.map(
+            (user,index)=>(
+                <li key={index}>{user.name}</li>
+            )
+          )
+        }
+      </ul>
     </div>
   )
 }
