621954a2a907cf9ca0d8ea05ba314ad99735f1b8
nasrin <n.mokhtari@wiki-land.net>
2025-03-15 14:22:01 +0330

    Client Component

--- newbynextjs/src/app/page.jsx
+++ newbynextjs/src/app/page.jsx
@@ -1,15 +1,12 @@
-import Link from "next/link";
-
+import ClientComponent from "./components/ClientComponent";
+import ServerComponent from "./components/ServerComponent";
 
 export default function Home() {
   return(
     <div>
       <h1>آکادمی نسرین مختاری</h1>
-      <Link href="/profile">
-            <button>
-              view profile  
-            </button>  
-      </Link>
+      <ServerComponent />
+      <ClientComponent />
     </div>
   )
 }
